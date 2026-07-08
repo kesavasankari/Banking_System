ba095e48e94809962decb9225a29af6469d32f07
kesavasankari <kesava.sankari@verinite.com>
2026-07-08 10:20:15 +0530

    Add files via upload

---
+++ banking system/api-gateway/api-gateway/src/main/java/com/example/api_gateway/util/JwtUtil.java
@@ -0,0 +1,47 @@
+package com.example.api_gateway.util;
+
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtUtil {
+
+    private static final String SECRET =
+            "mysecretkeymysecretkeymysecretkey123";
+
+    private final SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public boolean validateToken(String token) {
+
+        try {
+
+            Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token);
+
+            return true;
+
+        } catch (Exception e) {
+
+            return false;
+        }
+    }
+
+    public String extractUsername(String token) {
+
+        Claims claims = Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        return claims.getSubject();
+    }
+}
